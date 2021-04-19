584a7596e7721da47c1a9b40f7479b04fdab6650
Neeraj Sharma <64656303+Neerajsh8851@users.noreply.github.com>
2021-04-19 20:55:52 +0530

    Create GameFramework.java

---
+++ src/javagames/util/GameFramework.java
@@ -0,0 +1,6 @@
+public abstract class GameFramework extend JFrame implements Runnable {
+      protected void createFramework();
+      protected void renderFrame();
+      protected void getScreenWidth();
+      protected void getScreenHeight();
+}
